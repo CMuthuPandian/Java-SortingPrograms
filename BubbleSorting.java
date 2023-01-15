0f58eeb6cc2ef6f857a6eafac1e893a90cf0121b
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-15 11:31:03 +0530

    Update BubbleSorting.java

--- BubbleSorting.java
+++ BubbleSorting.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class BubbleSorting {
+    
     public static void main(String[] args) {
         int[] arr = new int[]{5, 8, 1, 10, 0, 3, 2};
         System.out.println(Arrays.toString(arr));
@@ -12,7 +13,6 @@ public class BubbleSorting {
     static void bubbleSort(int[] arr) {
         for(int i = 0; i <= arr.length - 1; ++i) {
             boolean swap = false;
-
             for(int j = 0; j < arr.length - i - 1; ++j) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
@@ -21,13 +21,12 @@ public class BubbleSorting {
                     swap = true;
                 }
             }
-
             if (!swap) {
                 break;
             }
         }
-
     }
+    
 }
 
 /*
