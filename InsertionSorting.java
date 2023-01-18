134802980c2e49e52b0f833f126cfa9e18ac9852
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-18 22:31:50 +0530

    Update InsertionSorting.java

--- InsertionSorting.java
+++ InsertionSorting.java
@@ -8,14 +8,12 @@ public class InsertionSorting {
         insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
-
     static void insertion(int[] arr) {
         for(int i = 0; i < arr.length - 1; ++i) {
             for(int j = i + 1; j > 0 && arr[j] < arr[j - 1]; --j) {
                 swap(arr, j, j - 1);
             }
         }
-
     }
     static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
