ce9ca509f159b225803066c06d09aaa6139f2cb2
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-19 23:31:15 +0530

    Add files via upload

---
+++ QuIckSorting.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class QuIckSorting {
+    public static void main(String[] args) {
+        int[] arr = new int[]{18, 7, 0, 3, 10, -1, 12};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int s = low;
+            int e = high;
+            int m = low + (high - low) / 2;
+            int pivot = nums[m];
+
+            while(s <= e) {
+                while(nums[s] < pivot) {
+                    ++s;
+                }
+
+                while(nums[e] > pivot) {
+                    --e;
+                }
+
+                if (s <= e) {
+                    int temp = nums[s];
+                    nums[s] = nums[e];
+                    nums[e] = temp;
+                    ++s;
+                    --e;
+                }
+
+                quickSort(nums, low, e);
+                quickSort(nums, s, high);
+            }
+
+        }
+    }
+
+}
