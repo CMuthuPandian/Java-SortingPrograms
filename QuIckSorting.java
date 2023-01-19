db0d25308fc63befc182004e5837c1137edbfa45
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-19 23:33:35 +0530

    Update QuIckSorting.java

--- QuIckSorting.java
+++ QuIckSorting.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 public class QuIckSorting {
     public static void main(String[] args) {
         int[] arr = new int[]{18, 7, 0, 3, 10, -1, 12};
@@ -12,16 +11,13 @@ public class QuIckSorting {
             int e = high;
             int m = low + (high - low) / 2;
             int pivot = nums[m];
-
             while(s <= e) {
                 while(nums[s] < pivot) {
                     ++s;
                 }
-
                 while(nums[e] > pivot) {
                     --e;
                 }
-
                 if (s <= e) {
                     int temp = nums[s];
                     nums[s] = nums[e];
@@ -29,7 +25,6 @@ public class QuIckSorting {
                     ++s;
                     --e;
                 }
-
                 quickSort(nums, low, e);
                 quickSort(nums, s, high);
             }
