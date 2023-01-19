11215636944c8a7a10d8e0513d2ea3422f5c90ad
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-19 23:33:05 +0530

    Update QuIckSorting.java

--- QuIckSorting.java
+++ QuIckSorting.java
@@ -6,7 +6,6 @@ public class QuIckSorting {
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
-
     static void quickSort(int[] nums, int low, int high) {
         if (low < high) {
             int s = low;
